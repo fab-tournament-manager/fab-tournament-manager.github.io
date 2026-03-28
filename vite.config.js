46dc3bbd2db19a9f0ac65bbf6b553d97c2acd757
raphaelSaraiva <raphael.saraiva13@gmail.com>
2026-03-27 22:04:08 -0300

    Configure GitHub Pages deploy

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+function resolveBase() {
+    var _a;
+    var repository = (_a = process.env.GITHUB_REPOSITORY) === null || _a === void 0 ? void 0 : _a.split('/')[1];
+    if (!repository) {
+        return '/';
+    }
+    if (repository.endsWith('.github.io')) {
+        return '/';
+    }
+    return "/".concat(repository, "/");
+}
 export default defineConfig({
+    base: resolveBase(),
     plugins: [react()],
 });
